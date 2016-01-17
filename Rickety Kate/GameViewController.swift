6960527b7b49a627323057cd53ce3993756faaf7
GeoffBurns <Geoffrey.Peter.Burns@gmail.com>
2016-01-18 07:49:58 +1100

    resize tidy

--- Rickety Kate/GameViewController.swift
+++ Rickety Kate/GameViewController.swift
@@ -17,23 +17,19 @@ class GameViewController: UIViewController , ADBannerViewDelegate {
     var adBannerView: ADBannerView = ADBannerView(frame: CGRect.zero)
 
     func loadAds(){
-        
-        
-    //    NSNotificationCenter.defaultCenter().addObserver(self, selector: "hideBannerAd", name: "hideadsID", object: nil)
-    //    NSNotificationCenter.defaultCenter().addObserver(self, selector: "showBannerAd", name: "showadsID", object: nil)
-
-
+  
         adBannerView.center = CGPoint(x: adBannerView.center.x, y: view.bounds.size.height - adBannerView.frame.size.height / 2)
         adBannerView.delegate = self
         adBannerView.hidden = true
         
-       // set self.canDisplayBannerAds to true 
-       // or use ADBannerView and ADBannerViewDelegate
-       // don't do both
-       // self.canDisplayBannerAds = true
+
         view.addSubview(adBannerView)
         
- //       NSNotificationCenter.defaultCenter().postNotificationName("showadsID", object: nil)
+        // set self.canDisplayBannerAds to true
+        // or use ADBannerView and ADBannerViewDelegate
+        // don't do both
+        // self.canDisplayBannerAds = true
+        
     }
 
     override func viewDidLoad() {
@@ -69,9 +65,9 @@ class GameViewController: UIViewController , ADBannerViewDelegate {
         /* Sprite Kit applies additional optimizations to improve rendering performance */
         skView.ignoresSiblingOrder = true
             
-        /* Set the scale mode to scale to fit the window */
-        
+        /* Set the scale mode to manually resize */
         scene.scaleMode = .ResizeFill
+        
         scene.table = RicketyKateCardTable.makeDemo(scene)
             
         skView.presentScene(scene)
@@ -123,33 +119,7 @@ class GameViewController: UIViewController , ADBannerViewDelegate {
      resize(adBannerView,size: size);
   
     }
-    
-    /*
-    func showBannerAd() {
-        adBannerView.hidden = false
-        resize(adBannerView)
-        let bannerHeight = adBannerView.bounds.height
-        
-        UIView.beginAnimations(nil, context: nil)
-        UIView.setAnimationDuration(1) // Time it takes the animation to complete
-        adBannerView.frame = CGRectMake(0, UIScreen.mainScreen().bounds.height - bannerHeight, 0, 0) // End position of the animation
-        UIView.commitAnimations()
-        NSLog("show Ad");
-    }
-    
-    func hideBannerAd() {
-        adBannerView.hidden = true
-        
-        let bannerHeight = adBannerView.bounds.height
-        UIView.beginAnimations(nil, context: nil)
-        UIView.setAnimationDuration(1) // Time it takes the animation to complete
-        adBannerView.frame = CGRectMake(0, UIScreen.mainScreen().bounds.height + bannerHeight, 0, 0) // End position of the animation
-        UIView.commitAnimations()
-        resize(nil)
-        NSLog("hide Ad");
-    }
 
-*/
 
    func resize(banner: ADBannerView?, size:CGSize = UIScreen.mainScreen().applicationFrame.size) {
         
@@ -190,10 +160,6 @@ class GameViewController: UIViewController , ADBannerViewDelegate {
     
     func bannerViewDidLoadAd(banner: ADBannerView!) {
        adBannerView.hidden = false
-     /*   UIView.beginAnimations(nil, context: nil)
-        UIView.setAnimationDuration(1) // Time it takes the animation to complete
-        adBannerView.alpha = 1 // Fade in the animation
-        UIView.commitAnimations() */
        resize(banner);
          NSLog("banner loaded");
     }
@@ -211,10 +177,7 @@ class GameViewController: UIViewController , ADBannerViewDelegate {
     
     func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
        adBannerView.hidden = true
-    /*    UIView.beginAnimations(nil, context: nil)
-        UIView.setAnimationDuration(1)
-        adBannerView.alpha = 0
-        UIView.commitAnimations()*/
+
         let status = banner.bannerLoaded ? "error but banner loaded" :"banner unloaded"
         NSLog(status);
         
