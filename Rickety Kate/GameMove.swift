8743908bbf2935ca57f852ee124e37dc1706fae2
Geoff Burns <Geoffrey.Peter.Burns@gmail.com>
2018-07-27 22:19:39 +1000

    GameMove fixed
    missing code replaced

--- Rickety Kate/GameMove.swift
+++ Rickety Kate/GameMove.swift
@@ -3,7 +3,41 @@
 //  Rickety Kate
 //
 //  Created by Geoff Burns on 27/7/18.
-//  Copyright © 2018 Nereids Gold. All rights reserved.
+//  Copyright © 2018 Geoff Burns. All rights reserved.
 //
 
+
 import Foundation
+import Cards
+
+public enum GameMove : Equatable
+{
+    case cardPlayed(CardHolderBase,PlayingCard)
+    case cardDoesNotFollowSuite(PlayingCard.Suite)
+    case trumpsHaveNotBeenBroken
+    
+    var description : String?
+    {
+        switch self
+        {
+        case .trumpsHaveNotBeenBroken :
+            return "Can not Lead with a %@".localizeWith(Game.moreSettings.rules.trumpSuiteSingular)
+        case .cardDoesNotFollowSuite( let suite )  :
+            return "Card Does Not Follow Suite".localize + "\n" + "Play a %@".localizeWith(suite.singular)
+            
+        case .cardPlayed :
+            return nil
+            
+        }
+    }
+    
+}
+
+
+public func ==(lhs: GameMove, rhs: GameMove) -> Bool {
+    switch (lhs, rhs) {
+        
+    case let (.cardDoesNotFollowSuite(la), .cardDoesNotFollowSuite(ra)): return la == ra
+    default: return false
+    }
+}
